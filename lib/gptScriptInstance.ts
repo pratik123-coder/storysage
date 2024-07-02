5e930c2520d255df14ffea887323ef3cc990341a
pratik123-coder <pratikmohanty939@yahoo.com>
2024-07-02 09:21:44 +0530

    fix: using model 4o

--- lib/gptScriptInstance.ts
+++ lib/gptScriptInstance.ts
@@ -1,8 +1,7 @@
 import { GPTScript } from "@gptscript-ai/gptscript";
 
 const g = new GPTScript({
-  APIKey: process.env.OPENAI_API_KEY,
-  DefaultModel: "gpt-3.5-turbo"
+  APIKey: process.env.OPENAI_API_KEY
 });
 
 export default g;
