93ea2e36a7657a4ec4f6c7e759f2467c86e95388
pratik123-coder <pratikmohanty939@yahoo.com>
2024-07-02 10:59:38 +0530

    fix: mobile view

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,21 +5,18 @@ import { BookOpen, FilePen } from "lucide-react";
 function Header() {
   return ( 
     <header className="relative p-16 text-center">
-      <Link href='/' >
-        <h1  className="text-6xl font-black pb-4">StorySage AI</h1>
-        <div className="flex justify-center space-x-5 text-3xl lg:text-5xl">
-          <h2>Generate Your Story Books</h2>
-          <div className="relative">
-            <div className="absolute bg-purple-500 -left-2 -top-1 
-            -bottom-1 -right-2 md:-left-3 md:-top-0 md:-bottom-0 
-            md:-right-3 -rotate-1" />
-            <p className="relative text-white">With AI</p>
-            
-            </div>
-            
-        </div>
-        <p className="pt-3">By Pratik Mohanty</p>
-      </Link>
+      <Link href='/'>
+  <h1 className="text-6xl font-black pb-4">StorySage AI</h1>
+  <div className="flex flex-col md:flex-row justify-center space-x-5 text-3xl lg:text-5xl">
+    <h2>Generate Your Story Books</h2>
+    <div className="relative">
+      <div className="absolute bg-purple-500 -left-2 -top-1 -bottom-1 -right-2 md:-left-3 md:-top-0 md:-bottom-0 md:-right-3 -rotate-1" />
+      <p className="relative text-white">With AI</p>
+    </div>
+  </div>
+  <p className="pt-3">By Pratik Mohanty</p>
+</Link>
+
       {/* Nav Icons */}
       <div className="absolute top-5 right-5 flex space-x-2 ">
         <Link href="/">
